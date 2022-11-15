b0eef02252dd0841943563f3328d1ed69f0b0931
Alex <alex@Mail.com>
2022-11-15 22:12:04 +0200

    initial

---
+++ task13/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Введите число с тремя и более цифрами ");
+
+
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+while (number < 100)
+    {System.Console.WriteLine("вы ввели число менее чем с тремя цифрами");
+        System.Console.Write("Введите число с тремя и более цифрами ");
+        number = Math.Abs(Convert.ToInt32(Console.ReadLine()));}
+
+
+    int a = number % 100;
+
+    number -= a;
+
+    System.Console.Write("третья цифра: " + (number % 1000) / 100);
