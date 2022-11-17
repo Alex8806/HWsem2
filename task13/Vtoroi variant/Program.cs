922f313b1c7949fcfc80626c90788ddb5c479323
Alex <alex@Mail.com>
2022-11-17 18:14:27 +0200

    votori varian

--- task13/Vtoroi variant/Program.cs
+++ task13/Vtoroi variant/Program.cs
@@ -7,11 +7,16 @@ string? chislo = Console.ReadLine();
 
 int i=Convert.ToInt32(chislo); // проверка число ли это
 i= chislo.Length;
-if ( i > 2 )
+
+if ( i > 2 && chislo[0] != '-' )
 {
    Console.WriteLine ("третья цифра: "+ chislo[2]);
 
 
+}
+else if (i>3 && chislo[0]=='-')
+{
+    Console.WriteLine ("третья цифра: "+ chislo[3]);
 }
 else
 {
