e59f3b6baf2534397280194d23473fe71c5d33ba
Alex <alex@Mail.com>
2022-11-17 17:47:27 +0200

    add tusk 13 vtoroi variant

---
+++ task13/Vtoroi variant/Program.cs
@@ -0,0 +1,19 @@
+﻿// Второй вариант
+
+Console.Write("Второй вариант, Введите число с тремя и более цифрами ");
+
+
+string? chislo = Console.ReadLine();
+
+int i=Convert.ToInt32(chislo); // проверка число ли это
+i= chislo.Length;
+if ( i > 2 )
+{
+   Console.WriteLine ("третья цифра: "+ chislo[2]);
+
+
+}
+else
+{
+    System.Console.WriteLine("третей цифры нет");
+}
